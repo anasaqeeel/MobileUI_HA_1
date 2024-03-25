cdd6b4e3a229d4320c346c874404399e487ceda3
anasaqeeel <122775288+anasaqeeel@users.noreply.github.com>
2024-03-26 00:16:44 +0500

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button supbtn = findViewById(R.id.exitbtn);
+        supbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Screen1.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
